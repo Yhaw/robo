a98ebb13de708cf69f023c4d3380d3cfe5c6211e
Yhaw <sylviankimkpe@yahoo.com>
2023-03-12 23:23:10 +0000

    first commit

---
+++ build/arduRover/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sparkrobotics/robo/src/arduRover
+# Build directory: /home/sparkrobotics/robo/build/arduRover
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sparkrobotics/robo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sparkrobotics/robo/devel;/home/sparkrobotics/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sparkrobotics/robo/devel;/home/sparkrobotics/catkin_ws/devel;/opt/ros/noetic' != '' else []
